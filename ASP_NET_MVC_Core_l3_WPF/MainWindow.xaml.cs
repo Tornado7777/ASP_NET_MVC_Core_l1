2bc546f86dda86fa31b94c788b742ffab424c9c8
Ivan <tornadod@mail.ru>
2022-10-21 19:13:04 +0300

    hw3

---
+++ ASP_NET_MVC_Core_l3_WPF/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ASP_NET_MVC_Core_l3_WPF
+{
+    /*
+     * Используя знания о паттернах, выберите приглянувшийся вам и создайте ICommand для WPF.
+     */
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
